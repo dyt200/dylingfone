39c3fb08d0ed6b55acb3074dd620ab7f05698c8e
dyt2000 <dy-thompson@hotmail.com>
2018-05-18 15:01:58 +0200

    First working message

--- src/dylingfone/MainTest.java
+++ src/dylingfone/MainTest.java
@@ -5,7 +5,7 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("helloooo");
+		System.out.print("hellooooo BEN");
 
 	}
 
