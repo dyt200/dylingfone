65cee0a01a8ec24a2fe7de9fd1b196ff59b9a9ff
dyt2000 <dy-thompson@hotmail.com>
2018-06-01 13:18:23 +0200

    Hello world test ben

--- src/dylingfone/Contacts.java
+++ src/dylingfone/Contacts.java
@@ -15,7 +15,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
+// HELLO WORLD
 public class Contacts extends Page{
 	private Contact[] contacts;
 	
