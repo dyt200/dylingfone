4f254985cb70829c2a03c8251d3fcae84c148565
Rabid <rabidmexican@gmail.com>
2018-06-14 20:11:30 +0200

    Added JUnit support

---
+++ jUnitTest/dylingfone/DisplayTest.java
@@ -0,0 +1,20 @@
+package dylingfone;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DisplayTest {
+	Display classBeingTested = new Display();
+	
+	@Test
+	void testAspectRatioCalculator() {
+		int expected = 321;
+		double x = 474.0;
+		double y = 474.0;
+		double z = 321.0;
+		
+		assertTrue(expected == classBeingTested.aspectRatioCalculator(x, y, z));
+	}
+
+}
---
+++ jUnitTest/dylingfone/StringFuncTest.java
@@ -0,0 +1,38 @@
+package dylingfone;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringFuncTest {
+	StringFunc classBeingTested;
+	
+	void setUp() {
+		classBeingTested = new StringFunc();
+	}
+	
+	@Test
+	void testIsValidDate() {
+		String testValue = "01.01.1970";
+		boolean expected = true;
+		
+		assertTrue(expected == classBeingTested.isValidDate(testValue));
+	}
+
+	@Test
+	void testIsValidEmail() {
+		String testValue = "pleaseGiveUs6@gmail.com";
+		boolean expected = true;
+		
+		assertTrue(expected == classBeingTested.isValidEmail(testValue));
+	}
+
+	@Test
+	void testIsValidPhoneNumber() {
+		String testValue = "+41123123123";
+		boolean expected = true;
+		
+		assertTrue(expected == classBeingTested.isValidPhoneNumber(testValue));
+	}
+
+}
